b87e1ab4d2019a46e27ad5b88cefc9494e6fd657
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-01 16:40:46 -0700

    Added char x parameter to dim_leds and changed void to char for green_on and red_on in stateMachines.h.

--- project/src/stateMachines.h
+++ project/src/stateMachines.h
@@ -2,9 +2,9 @@
 #define stateMachine_included
 
 void state_advance();
-void dim_leds();
-void green_on();
-void red_on();
+void dim_leds(char x);
+char green_on();
+char red_on();
 void buzzer_advance();
 
 #endif // included
