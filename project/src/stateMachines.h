0c7931a986a34844ae93cb84f32e045bd33db245
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-01 01:27:59 -0600

    copied stateMachines.h from blink_demo and put it in src of my project. Added dim_leds, green_on, red_on, and buzzer_advance declarations to stateMachines.h.

---
+++ project/src/stateMachines.h
@@ -0,0 +1,10 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+void dim_leds();
+void green_on();
+void red_on();
+void buzzer_advance();
+
+#endif // included
