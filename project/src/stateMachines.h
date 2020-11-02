6b6b2d6c7becb81f619bcc08e168fa4f02b7b323
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-01 17:26:15 -0700

    I had to change green_on and red_on to turn_green_on and turn_red_on in stateMachines.h.

--- project/src/stateMachines.h
+++ project/src/stateMachines.h
@@ -3,8 +3,8 @@
 
 void state_advance();
 void dim_leds(char x);
-char green_on();
-char red_on();
+char turn_green_on();
+char turn_red_on();
 void buzzer_advance();
 void state_up();
 void state_down();
